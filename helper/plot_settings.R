3f280153a3e1feadbd4a30bc209bccc8df4200f9
Mike Frank <mcfrank@stanford.edu>
2026-04-10 11:23:45 -0700

    minor cleanup

---
+++ helper/plot_settings.R
@@ -0,0 +1,46 @@
+library(ggplot2)
+library(ggthemes)
+
+.font <- "Source Sans 3"
+sysfonts::font_add_google(.font)
+showtext::showtext_auto()
+showtext::showtext_opts(dpi = 300)
+
+theme_set(theme_bw(base_size = 14, base_family = .font))
+theme_update(panel.grid = element_blank(),
+             strip.background = element_blank(),
+             legend.key = element_blank(),
+             panel.border = element_blank(),
+             axis.line = element_line(),
+             strip.text = element_text(face = "bold"))
+
+# set options for default continuous color and fill scales
+options("ggplot2.continuous.colour" = viridis::scale_colour_viridis)
+options("ggplot2.continuous.fill"   = viridis::scale_fill_viridis  )
+
+# color palette for tasks: ptol
+task_categories_vec <- c("Executive function", "Math", "Reasoning", "Spatial cognition", "Language", "Reading", "Social cognition")
+task_pal <- ptol_pal()(length(task_categories_vec)) |> rlang::set_names(task_categories_vec)
+.scale_colour_task <- \() scale_colour_manual(values = task_pal, ...)
+.scale_color_task <- .scale_colour_task
+
+# color palette for sites (solarized)
+sites <- c("pilot_uniandes_co", "pilot_mpieva_de", "pilot_western_ca")
+site_pal <- solarized_pal()(length(sites)) |> rlang::set_names(sites)
+.scale_colour_site <- \(...) scale_colour_manual(values = site_pal, ...)
+.scale_color_site <- .scale_colour_site
+
+# color pallete for other contrasts (brewer Set1)
+.scale_color_default <- \(...) scale_colour_brewer(palette = "Set1", ...)
+
+# specific element colors
+pal <- list(
+  grey = "#93a1a1",
+  red  = RColorBrewer::brewer.pal(3, "Set1")[1],
+  blue = RColorBrewer::brewer.pal(3, "Set1")[2]
+)
+
+# For mrot paper specifically
+stimuli <- c("duck", "rabbit", "shape")
+stimulus_pal <- RColorBrewer::brewer.pal(3, "Set1") |> rlang::set_names(stimuli)
+.scale_colour_stimulus <- \(...) scale_colour_manual(values = stimulus_pal, ...)
